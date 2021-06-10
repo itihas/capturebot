c7e641713cfb8ac2440f00ca3cdb9253558ce4a5
itihas <itihas@nixos>
2021-06-10 11:31:35 +0530

    working flake

--- flake.nix
+++ flake.nix
@@ -1,8 +1,31 @@
 {
-  outputs = { self, nixpkgs }: {
-    defaultPackage."x86_64-linux" = ./default.nix;
-    nixosModules = {
-      service = ./capturebot.nix;   
-    };
-  };
+  inputs.mach-nix.url = "github:DavHau/mach-nix";
+  outputs = { self, nixpkgs, mach-nix }:
+    let
+      system = "x86_64-linux";
+    in
+      rec {
+        defaultPackage.${system} = mach-nix.lib.${system}.buildPythonPackage {
+          pname = "capturebot";
+          version = "0.1";
+          src = ./. ;
+          requirements = ''python-telegram-bot'';
+
+        };
+
+        nixosModules = {
+          service = { pkgs,...}:
+            {
+              systemd.user.services.capturebot = {
+                path = [ defaultPackage.${system} pkgs.xdg_utils pkgs.emacs ];
+                script = "capturebot";
+                serviceConfig = {
+                  Type = "exec";
+                };
+                enable = true;
+                description = "org capture bot";
+              };
+            };
+        };
+      };
 }
