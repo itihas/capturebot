871f88205a8dd97627177bd3128b41c551cfd1ed
itihas <sahiti93@gmail.com>
2025-05-12 22:19:03 +0530

    add nixos-module.nix, which defines capturebot systemd service

--- flake.nix
+++ flake.nix
@@ -8,79 +8,63 @@
   };
 
   outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-      perSystem = { config, self', pkgs, lib, system, ... }:
-        let
-          runtimeDeps = with pkgs; [ openssl ];
-          buildDeps = with pkgs; [ pkg-config rustPlatform.bindgenHook ];
-          devDeps = with pkgs; [ gdb ];
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } ({ self, withSystem, flake-parts-lib, ... }:
+      let
+        inherit (flake-parts-lib) importApply;
+      in
+      {
+    imports = [  inputs.flake-parts.flakeModules.flakeModules ];
+    systems = [ "x86_64-linux" ];
+    perSystem = { config, self', pkgs, lib, system, ... }:
+    let
+      runtimeDeps = with pkgs; [ openssl ];
+      buildDeps = with pkgs; [ pkg-config rustPlatform.bindgenHook ];
+      devDeps = with pkgs; [ gdb ];
 
-          cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-          msrv = cargoToml.package.rust-version;
+      cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+      msrv = cargoToml.package.rust-version;
 
-          rustPackage = features:
-            (pkgs.makeRustPlatform {
-              cargo = pkgs.rust-bin.nightly.latest.minimal;
-              rustc = pkgs.rust-bin.nightly.latest.minimal;
-            }).buildRustPackage {
-              inherit (cargoToml.package) name version;
-              src = ./.;
-              cargoLock.lockFile = ./Cargo.lock;
-              buildFeatures = features;
-              buildInputs = runtimeDeps;
-              nativeBuildInputs = buildDeps;
-              # Uncomment if your cargo tests require networking or otherwise
-              # don't play nicely with the Nix build sandbox:
-              # doCheck = false;
-              # PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-            };
-
-          mkDevShell = rustc:
-            pkgs.mkShell {
-              shellHook = ''
-                export RUST_SRC_PATH=${pkgs.rustPlatform.rustLibSrc}
-              '';
-              buildInputs = runtimeDeps;
-              nativeBuildInputs = buildDeps ++ devDeps ++ [ rustc ];
-            };
-        in {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [ (import inputs.rust-overlay) ];
-          };
-
-          packages.default = self'.packages.capturebot;
-          devShells.default = self'.devShells.nightly;
-
-          packages.capturebot = (rustPackage "");
-
-          devShells.nightly = (mkDevShell (pkgs.rust-bin.selectLatestNightlyWith
-            (toolchain: toolchain.default)));
-          devShells.stable = (mkDevShell pkgs.rust-bin.stable.latest.default);
-          devShells.msrv = (mkDevShell pkgs.rust-bin.stable.${msrv}.default);
-        };
-      flake = {
-        nixosModule = {config, lib, pkgs, ...}: {
-          options.services.capturebot = {
-            enable = lib.mkEnableOption { description =  "enable the capturebot daemon"; };
-            botId = lib.mkOption {
-              type = lib.types.str;
-              default = null;
-              description = "Telegram Bot ID capturebot is a listener for.";
-            };
-            userId = lib.mkOption {
-              type = lib.types.int;
-              default = null;
-              description = "User ID that is going to be talking to capturebot.";
-            };
-            saveDir = lib.mkOption {
-              type = lib.types.path;
-              default = null;
-              description = "Path capturebot saves notes to";
-            };
-          };
-        };
+      rustPackage = features:
+      (pkgs.makeRustPlatform {
+        cargo = pkgs.rust-bin.nightly.latest.minimal;
+        rustc = pkgs.rust-bin.nightly.latest.minimal;
+      }).buildRustPackage {
+        inherit (cargoToml.package) name version;
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+        buildFeatures = features;
+        buildInputs = runtimeDeps;
+        nativeBuildInputs = buildDeps;
+        # Uncomment if your cargo tests require networking or otherwise
+        # don't play nicely with the Nix build sandbox:
+        # doCheck = false;
+        # PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
       };
+
+      mkDevShell = rustc:
+      pkgs.mkShell {
+        shellHook = ''
+          export RUST_SRC_PATH=${pkgs.rustPlatform.rustLibSrc}
+        '';
+        buildInputs = runtimeDeps;
+        nativeBuildInputs = buildDeps ++ devDeps ++ [ rustc ];
       };
+    in {
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [ (import inputs.rust-overlay) ];
+      };
+
+      packages.default = self'.packages.capturebot;
+      devShells.default = self'.devShells.nightly;
+
+      packages.capturebot = (rustPackage "");
+
+      devShells.nightly = (mkDevShell (pkgs.rust-bin.selectLatestNightlyWith
+      (toolchain: toolchain.default)));
+      devShells.stable = (mkDevShell pkgs.rust-bin.stable.latest.default);
+      devShells.msrv = (mkDevShell pkgs.rust-bin.stable.${msrv}.default);
+    };
+    flake.nixosModules.default = importApply ./nixos-module.nix { localFlake = self; inherit withSystem; };
+  });
 }
---
+++ nixos-module.nix
@@ -0,0 +1,46 @@
+{ localFlake, withSystem }:
+{config, lib, pkgs, ...}:
+with lib;
+let cfg = config.services.capturebot;
+in
+{
+  options.services.capturebot = {
+    enable = mkEnableOption { description =  "enable the capturebot daemon"; };
+    package = mkOption {
+      default = withSystem ({ config, ... }: config.packages.default);
+      defaultText = lib.literalMD "`packages.default` from the foo flake";
+    };
+    botId = mkOption {
+      type = types.str;
+      default = null;
+      description = "Telegram Bot ID capturebot is a listener for.";
+    };
+    userId = mkOption {
+      type = types.int;
+      default = null;
+      description = "User ID that is going to be talking to capturebot.";
+    };
+    saveDir = mkOption {
+      type = types.path;
+      default = null;
+      description = "Path capturebot saves notes to";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services.capturebot = {
+      name = "capturebot";
+      wantedBy = "network-online.target";
+      serviceConfig = {
+        ExecStart = "${cfg.package}/bin/capturebot";
+        RestartSec = 3;
+        Restart = "always";
+        RestartSteps = 3;
+      };
+      environment = {
+        "CAPTUREBOT_USER_ID" = cfg.userId;
+        "CAPTUREBOT_SAVE_DIR" = cfg.SaveDir;
+      };
+    };
+  };
+}
